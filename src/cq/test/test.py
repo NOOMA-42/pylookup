f1f4413a082fef092e62418bf0da11fd366c2a68
Harry <2411mail@gmail.com>
2024-01-08 22:23:47 +0700

    test: update test, add fk support

--- src/cq/test/test.py
+++ src/cq/test/test.py
@@ -1,7 +1,7 @@
-from setup import Setup
-from program import CommonPreprocessedInput
-from prover import Prover
 import random
+from src.cq.setup import Setup
+from src.cq.program import CommonPreprocessedInput
+from src.cq.prover import Prover
 
 # setup: public setup includes srs
 # public_table: public table
@@ -11,33 +11,15 @@ def prover(setup: Setup, public_table: [any], witness: [any]):
 
     print("table: ", public_table)
     print("witness: ", witness)
-    group_order_N = len(public_table)
     group_order_n = len(witness)
 
-    prover = Prover(setup, public_table, group_order_N, group_order_n)
+    prover = Prover(setup, public_table, group_order_n)
     proof = prover.prove(witness)
     print("Prover test success")
 
-    return proof, group_order_N, group_order_n
+    return proof
 
-def prover_simple_array(setup: Setup, public_table):
-    print("Beginning prover_simple_array test")
-    # values to lookup
-    witness = [1, 2, 1, 7, 3, 6, 3, 2]
-    proof, group_order_N, group_order_n = prover(setup, public_table, witness)
-    return proof, group_order_N, group_order_n
-
-def prover_random_lookup(setup: Setup, public_table):
-    print("Beginning prover_random_lookup test")
-    # values to lookup
-    witness = []
-    for _ in range(32):
-        witness.append(random.randint(1, 256))
-
-    proof, group_order_N, group_order_n = prover(setup, public_table, witness)
-    return proof, group_order_N, group_order_n
-
-def verifier(setup, proof, group_order_N, group_order_n):
+def verifier(setup: Setup, proof, group_order_N, group_order_n):
     print("Beginning verifier test")
     common_preprocessed_input = CommonPreprocessedInput(group_order_N, group_order_n)
     vk = setup.verification_key(common_preprocessed_input)
@@ -45,39 +27,52 @@ def verifier(setup, proof, group_order_N, group_order_n):
     print("Verifier test success")
 
 def simple_test():
+    print("===========> Beginning simple test ===========> ")
     # random number, normally comes from MPC(Multi-Party Computation)
     tau = 100
     # public table
-    table = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+    table = [1, 2, 3, 4]
+    # values to lookup
+    witness = [1, 2, 1, 3]
 
     group_order_N = len(table)
+    group_order_n = len(witness)
     # number of powers of tau
     powers = group_order_N * 2
     # do setup
     setup = Setup.execute(powers, tau, table)
     # run prover
-    proof, group_order_N, group_order_n = prover_simple_array(setup, table)
+    proof = prover(setup, table, witness)
     # run verifier
     verifier(setup, proof, group_order_N, group_order_n)
+    print("===========> End simple test ===========> ")
 
 def random_test():
+    print("===========> Beginning random test ===========> ")
     # random number, normally comes from MPC(Multi-Party Computation)
     tau = 100
 
     # public table
-    # table = [1...256]
+    # table = [1...32]
+    table_len = 32
     table = []
-    for i in range(1, 257):
+    for i in range(1, table_len + 1):
         table.append(i)
     print("table: ", table)
 
+    # values to lookup
+    witness = []
+    for _ in range(table_len):
+        witness.append(random.randint(1, table_len))
+
     group_order_N = len(table)
+    group_order_n = len(witness)
     # number of powers of tau
     powers = group_order_N * 2
     # do setup
     setup = Setup.execute(powers, tau, table)
     # run prover
-    proof, group_order_N, group_order_n = prover_random_lookup(setup, table)
+    proof = prover(setup, table, witness)
     # run verifier
     verifier(setup, proof, group_order_N, group_order_n)
 
